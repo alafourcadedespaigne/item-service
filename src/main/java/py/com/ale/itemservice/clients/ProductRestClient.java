87473314ebbf2415ce31bbae718d29835e2ac793
Alejandro Lafourcade <alafourcade@quarksoft.net>
2019-10-09 17:02:53 -0300

    LoadBalancer

--- src/main/java/py/com/ale/itemservice/clients/ProductRestClient.java
+++ src/main/java/py/com/ale/itemservice/clients/ProductRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import py.com.ale.itemservice.models.entity.Product;
 
-@FeignClient(name = "products-services", url = "localhost:8001")
+@FeignClient(name = "products-services")
 public interface ProductRestClient {
 
   @GetMapping("/listar")
